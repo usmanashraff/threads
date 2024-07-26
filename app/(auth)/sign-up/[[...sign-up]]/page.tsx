5f8d896c81f03454cb248c8468143fe80feab7dd
usmanashraff <osmanashruf@gmail.com>
2024-07-26 18:39:56 +0500

    fix onboarding error

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,6 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (<div className="bg-sign-in min-h-screen min-w-screen flex items-center justify-center">
-  <SignUp forceRedirectUrl='/onbaording' />;
+  <SignUp forceRedirectUrl='/onboarding' />;
 </div>)
 }
